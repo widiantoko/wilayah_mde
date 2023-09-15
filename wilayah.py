17c3a7149904bb35e12d63df63f99dddfc78e4c6
widiantoko <dodiwidiantoko@gmail.com>
2023-09-15 23:46:43 +0700

    anyar

--- wilayah.py
+++ wilayah.py
@@ -122,7 +122,7 @@ with col1:
       st.plotly_chart(fig9, use_container_width=True)
 
 
-      fig7 = go.Figure(data=go.Choroplethmapbox(geojson=geojson, 
+      fig7 = go.Figure(go.Choroplethmapbox(geojson=geojson, 
                                     locations=df3["distrik"], z=df3['konid'],
                                     colorscale="Viridis", marker_line_width=.5))
 
