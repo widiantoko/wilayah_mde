250a35e848cb89e769911149d75f0c4c89674960
widiantoko <dodiwidiantoko@gmail.com>
2023-09-15 20:42:30 +0700

    comm

--- wilayah.py
+++ wilayah.py
@@ -104,13 +104,11 @@ with col1:
       fig9 = px.choropleth_mapbox(df3, geojson=geojson,
                                   locations=df3["distrik"], 
                      featureidkey="properties.WADMKC",color=df3["konid"], 
-      mapbox_style="carto-positron", mapbox_center = {"lat": -6.234497, "lon": 106.805833},
+      mapbox_style="carto-positron", 
                   mapbox_zoom=3,width=700,height=750,
                      )#type: ignore 
 
-      fig9.update_layout(  margin=dict(l=1, r=1, t=1, b=1), title= f"""<br>Sebaran Data Dokumen UOB di Kecamatan DKI Jakarta 
-                   <br>(n : {n} dari {all} Dokumen) """, autosize=True, 
-                 title_y=0.9, title_font_size=22, title_yanchor="top", title_xanchor='left' ,margin_t=50, showlegend=True)
+      fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
       
     
 
