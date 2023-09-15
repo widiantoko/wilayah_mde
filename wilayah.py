78400c9024489ffb5bfea25f0e3181b81364472c
widiantoko <dodiwidiantoko@gmail.com>
2023-09-16 00:03:06 +0700

    zoom

--- wilayah.py
+++ wilayah.py
@@ -130,8 +130,8 @@ with col1:
               
                         height = 600,
                         autosize=True,
-                        margin={"r":0,"t":0,"l":0,"b":0},
-                        mapbox=dict(center=dict(lat=-6.202905, lon=106.778419),zoom=9),
+                        margin={"r":0,"t":0,"l":0,"b":0}, mapbox_center = {"lat":-6.202905, "lon": 106.778419},
+                        mapbox_zoom=9,
                         )
 
 
