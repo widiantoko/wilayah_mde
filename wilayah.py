152e8ea52640ea1bfb1d797466c906d572648c58
widiantoko <dodiwidiantoko@gmail.com>
2023-09-16 07:19:48 +0700

    ya

--- wilayah.py
+++ wilayah.py
@@ -114,9 +114,9 @@ with col1:
 
                   
                      )#type: ignore 
-
-      fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
       fig9.update_traces(hovertemplate="<b>{custom_data}  Kiriman</b>")
+      
+      fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
       
     
 
