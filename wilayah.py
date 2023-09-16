a7a5ffb5afccd33ff8cbc5e1333af365d82d60de
widiantoko <dodiwidiantoko@gmail.com>
2023-09-16 07:06:05 +0700

    test aza

--- wilayah.py
+++ wilayah.py
@@ -116,7 +116,7 @@ with col1:
                      )#type: ignore 
 
       fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-      fig9.update_traces(hovertemplate="<b>%{custom_data[0]} : %{custom_data[1]} Kiriman</b>")
+      fig9.update_traces(hovertemplate="<b>%{custom_data}  Kiriman</b>")
       
     
 
