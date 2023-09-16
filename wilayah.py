ab73db2bcc4b5cd75214ac48a39b35c1b66cf9fc
widiantoko <dodiwidiantoko@gmail.com>
2023-09-16 08:58:28 +0700

    ayo

--- wilayah.py
+++ wilayah.py
@@ -38,7 +38,8 @@ df3 = pd.merge(pd.merge(df, df2, on='kec', how='left'), p_table, on='kec', how='
 df3["sukses"]=round(df3["Y"] / df3["konid"] * 100,2)
 df3["failed"]=round(df3["C"] / df3["konid"] * 100,2)
 df3["no_status"]=round(df3["empty"] / df3["konid"] * 100,2)
-df3["judul"]=df3["distrik"].astype(str)+ " : " + df3["konid"].astype(str) + "Dokumen"
+df3["judul"]=df3["distrik"].astype(str)+ " : " + df3["konid"].astype(str) + " Dokumen"
+df3["sukses_1"]= "Sukses : " + df3["Y"].astype(str)+ " ("+ df3["sukses"].astype(str)+" %)"
 
 n=df3["konid"].sum()
 na=jkt['kec'].isna().sum()
@@ -111,7 +112,7 @@ with col1:
                            zoom=10, center = {"lat": -6.202905, "lon": 106.778419},
                            opacity=0.5, height=700,
                            hover_name="judul",
-                           hover_data = {'konid':False, 'distrik':False}
+                           hover_data = {'konid':False, 'distrik':False, "sukses_1": True}
                      
                     
                            
