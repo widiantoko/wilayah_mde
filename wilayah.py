324027119e7b426cbc77a4c9bf8a22a3ca5c7b42
widiantoko <dodiwidiantoko@gmail.com>
2023-09-16 09:02:06 +0700

    coba ah

--- wilayah.py
+++ wilayah.py
@@ -39,7 +39,7 @@ df3["sukses"]=round(df3["Y"] / df3["konid"] * 100,2)
 df3["failed"]=round(df3["C"] / df3["konid"] * 100,2)
 df3["no_status"]=round(df3["empty"] / df3["konid"] * 100,2)
 df3["judul"]=df3["distrik"].astype(str)+ " : " + df3["konid"].astype(str) + " Dokumen"
-df3["sukses_1"]= "Sukses : " + df3["Y"].astype(str)+ " ("+ df3["sukses"].astype(str)+" %)"
+df3["Sukses"]= df3["Y"].astype(str)+ " ("+ df3["sukses"].astype(str)+" %)"
 
 n=df3["konid"].sum()
 na=jkt['kec'].isna().sum()
@@ -112,7 +112,7 @@ with col1:
                            zoom=10, center = {"lat": -6.202905, "lon": 106.778419},
                            opacity=0.5, height=700,
                            hover_name="judul",
-                           hover_data = {'konid':False, 'distrik':False, "sukses_1": True}
+                           hover_data = {'konid':False, 'distrik':False, "Sukses": True}
                      
                     
                            
