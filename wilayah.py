758791629f86b7e4f1e61a0e0f3e78ece74c7c98
widiantoko <dodiwidiantoko@gmail.com>
2023-10-04 20:34:20 +0700

    kbn

--- wilayah.py
+++ wilayah.py
@@ -27,7 +27,7 @@ df = pd.DataFrame(
 for kec in df['kec'].to_list():
   jkt.loc[ jkt['join'].str.contains(kec), 'kec'] = kec
 
-kec_2=[["PALMERAH", "PAL MERAH"], ["KB JERUK", "KEBON JERUK"]]
+kec_2=[["PALMERAH", "PAL MERAH"], ["KBN JERUK", "KEBON JERUK"]]
 df_kec=pd.DataFrame(kec_2, columns=['alias', 'kec_alias'])
 
 for alias in df_kec['alias'].to_list():
