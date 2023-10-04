ac9a64880adb62d040ea6fa02e1d489c686c9055
widiantoko <dodiwidiantoko@gmail.com>
2023-10-04 20:03:58 +0700

    jkt

--- wilayah.py
+++ wilayah.py
@@ -30,7 +30,7 @@ for kec in df['kec'].to_list():
 
 p_table = pd.pivot_table(jkt, index= ['kec'],  columns=['pod'], values='konid', aggfunc = 'count' ).fillna(0).reset_index()
 
-st.table(p_table)
+st.dataframe(jkt)
 
 df2 = jkt.groupby(['kec'], as_index=False)['konid'].count()
 df2a= jkt.groupby(['kec', 'pod'], as_index=False)['konid'].count()
