012badb944050433ffabecdf8acadd09e36a1860
widiantoko <dodiwidiantoko@gmail.com>
2023-10-04 23:04:51 +0700

    empty

--- wilayah.py
+++ wilayah.py
@@ -51,7 +51,7 @@ for kec in df['kec'].to_list():
 
 
 #st.dataframe(df_kec)
-st.dataframe(jkt['kec'].isna)
+st.dataframe(jkt['kec'==""])
 
 
 p_table = pd.pivot_table(jkt, index= ['kec'],  columns=['pod'], values='konid', aggfunc = 'count' ).fillna(0).reset_index()
