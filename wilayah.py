cce9c923b0fa5c6f8ab8680b859dc12e51043c73
widiantoko <dodiwidiantoko@gmail.com>
2023-10-05 05:39:48 +0700

    grogol

--- wilayah.py
+++ wilayah.py
@@ -86,7 +86,7 @@ for kec in df['kec'].to_list():
 
 
 kec_none=jkt.loc[jkt['kec'].isnull()].sort_values(by=['kab'], ascending=False)
-kec_pilih=jkt.loc[jkt['join'].str.contains("Grogol")].sort_values(by=['kab'], ascending=False)
+kec_pilih=jkt[jkt['join'=="Grogol"]].sort_values(by=['kab'], ascending=False)
 
 #st.dataframe(df_kec)
 st.dataframe(kec_pilih)
