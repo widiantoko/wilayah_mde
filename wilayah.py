7bf3cb808f6d7b10576d2b4127c276a3bb933034
widiantoko <dodiwidiantoko@gmail.com>
2023-10-05 10:30:37 +0700

    C

--- wilayah.py
+++ wilayah.py
@@ -92,11 +92,11 @@ for kec in df['kec'].to_list():
 
 
 kec_none=jkt.loc[jkt['kec'].isnull()].sort_values(by=['kab'], ascending=False)
-kec_pilih=jkt[jkt['join'].str.contains("grogol|Grogol",  case = False)]
+kec_pilih=jkt[jkt['join'].str.contains("SUDIRMAN",  case = False)]
 #kec_pilih=jkt.set_index('join').filter(like='grogol', axis=0)
 
 #st.dataframe(df_kec)
-st.dataframe(kec_none)
+st.dataframe(kec_PILIH)
 
 
 
