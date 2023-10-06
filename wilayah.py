6adc0aa687b64376e22c10785fabd11f3b9e1747
widiantoko <dodiwidiantoko@gmail.com>
2023-10-06 15:18:37 +0700

    NO

--- wilayah.py
+++ wilayah.py
@@ -198,7 +198,7 @@ kec_pilih=jkt[(jkt['join'].str.contains("GADING",  na = False, case=False)) & (j
 
 #kec_cth=jkt[jkt['alam6'].str.contains('KEBAYORAN|KEB',  na = False, case=False)]
 
-st.dataframe(kec_pilih)
+st.dataframe(kec_none)
 
 
 p_table = pd.pivot_table(jkt, index= ['kec'],  columns=['pod'], values='konid', aggfunc = 'count' ).fillna(0).reset_index()
