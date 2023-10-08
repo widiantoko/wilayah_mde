170e66802e79c01131ab664aa1e42cffb90f48de
widiantoko <dodiwidiantoko@gmail.com>
2023-10-08 11:39:51 +0700

    PILIH

--- wilayah.py
+++ wilayah.py
@@ -323,12 +323,12 @@ for kec in df['kec'].to_list():
 
 
 kec_none=jkt.loc[jkt['kec'].isnull()].sort_values(by=['kab'], ascending=False)
-kec_pilih=jkt[(jkt['join'].str.contains("RUKAN",  na = False, case=False)) & (jkt['kec'].isnull())]
+kec_pilih=jkt[(jkt['join'].str.contains("HAYAM",  na = False, case=False)) & (jkt['kec'].isnull())]
 
 
 
 
-st.dataframe(kec_none)
+st.dataframe(kec_pilih)
 
 
 p_table = pd.pivot_table(jkt, index= ['kec'],  columns=['pod'], values='konid', aggfunc = 'count' ).fillna(0).reset_index()
