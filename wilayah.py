0677ac49b1ec527285b8c7070fcec3621d26dfb4
widiantoko <dodiwidiantoko@gmail.com>
2023-10-18 16:22:21 +0700

    coba

--- wilayah.py
+++ wilayah.py
@@ -430,7 +430,7 @@ with col1:
                                     
                           
                      )#type: ignore 
-      fig9.update_traces(hovertemplate="judul")
+      fig9.update_traces(hovertemplate = "%{judul}")
       
       fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
       fig9.update_layout(
