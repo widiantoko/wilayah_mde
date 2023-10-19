0b261595271c543d010e5d44e0e1e42f54364349
widiantoko <dodiwidiantoko@gmail.com>
2023-10-19 17:10:15 +0700

    okeh

--- wilayah.py
+++ wilayah.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Kiriman Jakarta", layout='wide')
 
 col1, col2 = st.columns([5,2] ,gap="small")
 
-jkt=pd.read_excel("data/UOB_Jkt_Okt.xlsx")
+jkt=pd.read_excel("data/baru.xlsx")
 jkt["join"]=jkt["alam4"].astype(str) +" " +jkt["alam5"].astype(str) +" " + jkt["alam6"].astype(str)
 jkt['pod'].fillna("empty",inplace=True)
 
