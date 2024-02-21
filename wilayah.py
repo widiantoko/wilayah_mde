55f87b42b80a70e1b655748ea3baa896f977266f
widiantoko <dodiwidiantoko@gmail.com>
2024-02-21 15:00:34 +0700

    ayo

--- wilayah.py
+++ wilayah.py
@@ -295,6 +295,7 @@ jkt['join'] = jkt['join'].str.replace('CIPINANG MUARA', 'JATINEGARA')
 jkt['join'] = jkt['join'].str.replace('RAWA BUNGA', 'JATINEGARA')
 jkt['join'] = jkt['join'].str.replace('TANJUNG BARAT', 'JAGAKARSA')
 jkt['join'] = jkt['join'].str.replace('KOTA WISATA CIBUBUR', 'CIRACAS')
+jkt['join'] = jkt['join'].str.replace('LINDETEVES', 'MANGGA BESAR')
 
 
 
