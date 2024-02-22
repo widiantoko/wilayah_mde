3b21a504e1a2d70e6163bef8f95746265d888524
widiantoko <dodiwidiantoko@gmail.com>
2024-02-22 13:30:57 +0700

    okt

--- wilayah.py
+++ wilayah.py
@@ -536,7 +536,8 @@ with col1:
 )
       
     
-      st.subheader("Sebaran Kiriman Jakarta Per Kecamatann Per Bulan (Okt 2023 s.d Feb 2024) ")
+      st.subheader("Sebaran Kiriman Jakarta Per Kecamatan Per Bulan")
+      st.subheader("Periode : Okt 2023 s.d Feb 2024")
       st.plotly_chart(fig9, use_container_width=True)
 
 
