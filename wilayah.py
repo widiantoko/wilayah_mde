bd65ae0d1ef585c445835c9b1cf0e38432a967c3
widiantoko <dodiwidiantoko@gmail.com>
2024-02-22 11:03:54 +0700

    600

--- wilayah.py
+++ wilayah.py
@@ -502,7 +502,7 @@ with col1:
                            range_color=(0, 1300),
                            mapbox_style="carto-positron",
                            zoom=10, center = {"lat": -6.202905, "lon": 106.778419},
-                           opacity=0.7, height=700,
+                           opacity=0.7, height=600,
                            hover_name="judul",
                            hover_data = {'konid':False, 'distrik':False, "Sukses": True, "Gagal":True, "No Status":True}
                                     
