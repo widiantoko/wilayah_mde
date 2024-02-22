749ddc2c247d8302785476365973c73975864dfa
widiantoko <dodiwidiantoko@gmail.com>
2024-02-22 17:12:52 +0700

    upper

--- wilayah.py
+++ wilayah.py
@@ -14,6 +14,7 @@ col1, col2 = st.columns([5,2] ,gap="small")
 jkt=pd.read_csv("data/baru.csv")
 
 jkt["join"]=jkt["alam4"].astype(str) +" "+jkt["alam5"].astype(str) +" "+jkt["alam6"].astype(str)
+jkt['join'] = jkt['join'].str.upper() 
 
 jkt['pod'].fillna("empty",inplace=True)
 
