cd73baa842fdaf60f807efe570f1f136ab8c0ccc
widiantoko <dodiwidiantoko@gmail.com>
2024-02-23 20:39:26 +0700

    yy

--- wilayah.py
+++ wilayah.py
@@ -100,7 +100,7 @@ with col1:
 
       st.plotly_chart(fig9, use_container_width=True)
   
-      st.text(f"Jumlah Kiriman UOB {pilihan} : {all} Dokumen")
+      st.markdown(f"Jumlah Kiriman UOB {pilihan} : *{all}* Dokumen")
       st.markdown(f"**{n}** / **{na}**")
       st.markdown(f"**{pod_Y}** / **{pod_C}** / **{pod_empty}**")
 
@@ -184,7 +184,7 @@ with col2:
 
       st.plotly_chart(fig4, use_container_width=True)
   
-      st.markdown(f"Jumlah Kiriman UOB {pilihan_A} : {all4} Dokumen")
+      st.markdown(f"Jumlah Kiriman UOB {pilihan_A} : **{all4}** Dokumen")
       st.markdown(f"**{n4}** / **{na4}**")
       st.markdown(f"**{pod_Y4}** / **{pod_C4}** / **{pod_empty4}**")
 
